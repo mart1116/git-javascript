f101320ffa4b4c06ddd6caf1c7bd56746a2b2548
Kat Martinez <mart1116@algonquinlive.com>
2020-01-12 23:27:00 -0500

    added math

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 var name = 'Kat'
 
-console.log('name')
+console.log(hi my name is Kat)
+
+var number = 6
+console.log("number" * 2)
+console.log(12 * 5)
+console.log(60 / "number")
+console.log(10 - 7)
